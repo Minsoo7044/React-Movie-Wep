ffe57c8f69f89fbf10d5072dfc738645b93f9060
minsoo <als494@naver.com>
2022-05-26 01:32:03 +0900

    delete button

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState('');  
+  const [toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value); 
+  const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
     if (toDo === "") {
       return;
     }
-    setToDos((currnetArray) => [toDo, ...currnetArray]);
+    setToDos([...toDos, toDo]);
     setToDo("");
-  }
+  };
   return (
     <div>
       <h2>My To Dos ({toDos.length})</h2>
       <form onSubmit={onSubmit}>
-        <input 
+        <input
           onChange={onChange}
           value={toDo}
           type="text"
-          placeholder='Write your to do ...'
+          placeholder="Write your to do ..."
         />
         <button>Add To Do </button>
       </form>
       <hr />
       <ul>
-        {toDos.map((item, index) => <li key={index}>{item}</li>)}
+        {toDos.map((item, index) => (
+          <li key={index}>
+            {item}{" "}
+            <button onClick={() => toDos.splice(index, 1)}>delete</button>
+          </li>
+        ))}
       </ul>
     </div>
   );
